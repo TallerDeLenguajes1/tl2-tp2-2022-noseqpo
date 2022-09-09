11d60ab48d9eeedb8a1dfeee2f39b85b1475b70d
Daniel Paz <danielnicolaspeace@gmail.com>
2022-09-09 18:49:59 -0300

    anda nlog y algunas clases

---
+++ TP02/TP02/InterfazDatos.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TP02
+{
+    public class InterfazDatos
+    {
+        private static readonly NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+
+
+        public void CargarAlumno()
+        {
+            Alumno temp = new Alumno();
+
+            temp.Nombre = Texto("nombre");
+            temp.Apellido = Texto("apellido");
+            temp.Dni = Numero("DNI");
+            temp.Curso = Numero("curso");
+
+        }
+
+        private string Texto(string atributo)
+        {
+            string temp = "";
+
+            Console.WriteLine("Ingrese " + atributo + ":");
+
+            try
+            {
+                temp = Console.ReadLine();
+            }
+            catch(Exception ex)
+            {
+                logger.Info(ex, "Eror en Texto");
+            }
+
+            return temp;
+        }
+
+        private int Numero(string atributo)
+        {
+            int n = 0;
+            string temp = Texto(atributo);
+
+            try
+            {
+                n = Int32.Parse(temp);
+            }
+            catch(FormatException ex)
+            {
+                logger.Info(ex, "Error en formato");
+            }
+
+            return n;
+        }
+
+    }
+}
--- TP02/TP02/Program.cs
+++ TP02/TP02/Program.cs
@@ -1,3 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP02
+{
+    public static class Program
+    {
+        private static readonly NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+
+        public static void Main()
+        {
+            try
+            {
+                logger.Info("Hello world");
+                System.Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, "Goodbye cruel world");
+            }
+
+            InterfazDatos instituto = new InterfazDatos();
+
+            instituto.CargarAlumno();
+        }
+    }
+}
 
